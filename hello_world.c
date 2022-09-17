8271fd6e858be82baf1b2376760e9b08eb56379d
Daniel Addotey <addoteyd@gmail.com>
2022-09-17 16:34:13 +0000

    Hello World in C

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints Hello World to screen
+ * 
+ * Return: On success 0.
+*/
+
+int main(void)
+{
+	printf("Hello, World!\n");
+	return EXIT_SUCCESS;
+}
